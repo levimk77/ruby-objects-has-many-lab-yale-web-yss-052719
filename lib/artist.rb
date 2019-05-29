c038ee8c7d5e1ff177dacbf07983589afdb78999
Elijah Florence <efloren@siue.edu>
2019-05-29 20:13:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist 
   attr_accessor :name, :songs
-  
+  @@songCount = 0
  
   def initialize(name)
     @name = name
@@ -8,11 +8,13 @@ class Artist
   end
   
   def add_song(song)
+  @@songCount = @@songCount + 1
   song.artist = self
   @songs << song
   end
   
   def add_song_by_name(name)
+    @@songCount = @@songCount + 1
     song = Song.new(name)
     song.artist = self
     @songs << song
