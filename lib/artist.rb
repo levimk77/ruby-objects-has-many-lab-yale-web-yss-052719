ed5f8a39662180d88ce50c056d821e739a759ee8
Elijah Florence <efloren@siue.edu>
2019-05-29 20:12:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,22 +4,22 @@ class Artist
  
   def initialize(name)
     @name = name
-    @@songs = []
+    @songs = []
   end
   
   def add_song(song)
   song.artist = self
-  @@songs << song
+  @songs << song
   end
   
   def add_song_by_name(name)
     song = Song.new(name)
     song.artist = self
-    @@songs << song
+    @songs << song
   end
   
   def self.song_count
-    @@songs.count 
+    @songs.count 
   end
   
   
